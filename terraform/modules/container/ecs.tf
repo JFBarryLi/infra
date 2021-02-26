a2cf6b89d0553a5cefb57dff055e9e33eff85c6b
Barry Li <bli031@uottawa.ca>
2021-02-26 03:29:56 +0000

    Updated ecs init service.

--- terraform/modules/container/ecs.tf
+++ terraform/modules/container/ecs.tf
@@ -7,13 +7,11 @@ resource "aws_ecs_task_definition" "init" {
   container_definitions = file("placeholder_task_def.json")
 }
 
-resource "aws_ecs_service" "mongo" {
-  name            = "mongodb"
+resource "aws_ecs_service" "init" {
+  name            = "init"
   cluster         = aws_ecs_cluster.this.id
   task_definition = aws_ecs_task_definition.init.arn
-  desired_count   = 0
-  iam_role        = aws_iam_role.ecs.arn
-  depends_on      = [aws_iam_role_policy.ecs]
+  desired_count   = 1
 
   placement_constraints {
     type       = "memberOf"
