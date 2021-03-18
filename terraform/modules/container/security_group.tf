de0aae4ef36cd8b1ca333f73bc5e8bf8127c176a
Barry Li <bli031@uottawa.ca>
2021-03-18 03:27:17 +0000

    Updated ecs and alb security groups.

--- terraform/modules/container/security_group.tf
+++ terraform/modules/container/security_group.tf
@@ -1,24 +1,39 @@
+resource "aws_security_group" "alb" {
+  name   = "${var.ecs_cluster_name}-alb-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    description = "Alb allow https."
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_security_group" "ecs" {
-    vpc_id = var.vpc_id
+  name   = "${var.ecs_cluster_name}-ecs-sg"
+  vpc_id = var.vpc_id
+}
 
-    ingress {
-        from_port       = 22
-        to_port         = 22
-        protocol        = "tcp"
-        cidr_blocks     = ["0.0.0.0/0"]
-    }
+resource "aws_security_group_rule" "ecs_allow_https_from_alb" {
+  description       = "Ecs allow https from alb."
+  security_group_id = aws_security_group.ecs.id
 
-    ingress {
-        from_port       = 443
-        to_port         = 443
-        protocol        = "tcp"
-        cidr_blocks     = ["0.0.0.0/0"]
-    }
+  type                     = "ingress"
+  from_port                = var.container_port
+  to_port                  = var.container_port
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.alb.id
+}
 
-    egress {
-        from_port       = 0
-        to_port         = 65535
-        protocol        = "tcp"
-        cidr_blocks     = ["0.0.0.0/0"]
-    }
+resource "aws_security_group_rule" "ecs_allow_outbound" {
+  description       = "Ecs allow outbound."
+  security_group_id = aws_security_group.ecs.id
+
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
 }
