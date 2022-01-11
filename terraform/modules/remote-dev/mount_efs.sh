fa63701ef2cbabb39cf9a14594b90f29df3e6be2
Barry <bli031@uottawa.ca>
2022-01-11 13:18:20 +0000

    Updated efs mount point to home dir.

--- terraform/modules/remote-dev/mount_efs.sh
+++ terraform/modules/remote-dev/mount_efs.sh
@@ -4,9 +4,7 @@ apt-get install -y nfs-common
 apt-get install -y amazon-efs-utils
 apt-get install -y nfs-utils
 
-mkdir -p /mnt/efs/fs1
-
-test -f "/sbin/mount.efs" && printf "\n fs-054467034aa1be2b1:/ /mnt/efs/fs1 efs tls,_netdev\n" >> /etc/fstab || printf "\n${efs_id}.efs.${region}.amazonaws.com:/ /mnt/efs/fs1 nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport,_netdev 0 0\n" >> /etc/fstab
+test -f "/sbin/mount.efs" && printf "\n fs-054467034aa1be2b1:/ /home/ubuntu efs tls,_netdev\n" >> /etc/fstab || printf "\n${efs_id}.efs.${region}.amazonaws.com:/ /home/ubuntu nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport,_netdev 0 0\n" >> /etc/fstab
 
 test -f "/sbin/mount.efs" && grep -ozP 'client-info]\nsource' '/etc/amazon/efs/efs-utils.conf'; if [[ $? == 1 ]]; then printf "\n[client-info]\nsource=liw\n" >> /etc/amazon/efs/efs-utils.conf; fi;
 
