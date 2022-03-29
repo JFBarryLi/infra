f5a5fcd94521f71a4567634ff6e7768a5668cfde
Barry <bli031@uottawa.ca>
2022-03-29 13:39:11 +0000

    Getting started with serverless pipeline.

---
+++ terraform/modules/serverless-pipeline/README.md
@@ -0,0 +1,44 @@
+# Serverless Data Pipeline
+
+<!-- BEGIN_TF_DOCS -->
+## Requirements
+
+No requirements.
+
+## Providers
+
+| Name | Version |
+|------|---------|
+| <a name="provider_aws"></a> [aws](#provider\_aws) | n/a |
+
+## Modules
+
+No modules.
+
+## Resources
+
+| Name | Type |
+|------|------|
+| [aws_dynamodb_table.state](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/dynamodb_table) | resource |
+| [aws_s3_bucket.state](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket) | resource |
+| [aws_s3_bucket_policy.state](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_policy) | resource |
+| [aws_s3_bucket_public_access_block.state](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_public_access_block) | resource |
+| [aws_iam_policy_document.state](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/iam_policy_document) | data source |
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:--------:|
+| <a name="input_create_lock_table"></a> [create\_lock\_table](#input\_create\_lock\_table) | Whether or not to create a Dynamo DB lock table | `bool` | `false` | no |
+| <a name="input_log_bucket_name"></a> [log\_bucket\_name](#input\_log\_bucket\_name) | The name of the S3 bucket to write logs to. | `string` | n/a | yes |
+| <a name="input_state_bucket_name"></a> [state\_bucket\_name](#input\_state\_bucket\_name) | The name of the S3 bucket to write the state to. | `string` | n/a | yes |
+
+## Outputs
+
+| Name | Description |
+|------|-------------|
+| <a name="output_bucket_state_arn"></a> [bucket\_state\_arn](#output\_bucket\_state\_arn) | The arn of the created remote state bucket. |
+| <a name="output_bucket_state_name"></a> [bucket\_state\_name](#output\_bucket\_state\_name) | The name of the created remote state bucket. |
+| <a name="output_lock_table_arn"></a> [lock\_table\_arn](#output\_lock\_table\_arn) | The arn of the created dynamodb state lock table. |
+| <a name="output_lock_table_name"></a> [lock\_table\_name](#output\_lock\_table\_name) | The name of the created dynamodb state lock table. |
+<!-- END_TF_DOCS -->
---
+++ terraform/modules/serverless-pipeline/s3.tf
@@ -0,0 +1,140 @@
+resource "aws_s3_bucket_public_access_block" "pipeline" {
+  bucket = aws_s3_bucket.pipeline.id
+
+  ignore_public_acls      = true
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket" "pipeline" {
+  bucket        = var.pipeline_bucket_name
+  acl           = "private"
+  force_destroy = false
+
+  versioning {
+    enabled = true
+  }
+
+  logging {
+    target_bucket = var.log_bucket_name
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  lifecycle_rule {
+    id                                     = "${var.pipeline_bucket_name}-lifecycle"
+    enabled                                = true
+    abort_incomplete_multipart_upload_days = 1
+
+    noncurrent_version_transition {
+      days          = "30"
+      storage_class = "GLACIER"
+    }
+  }
+
+  tags = {
+    Name      = var.pipeline_bucket_name
+    ManagedBy = "terraform"
+  }
+}
+
+resource "aws_s3_bucket_policy" "pipeline" {
+  bucket = aws_s3_bucket.pipeline.id
+  policy = data.aws_iam_policy_document.pipeline.json
+}
+
+data "aws_iam_policy_document" "pipeline" {
+  statement {
+    sid    = "DenyNotBucketOwnerFullControl"
+    effect = "Deny"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:PutObject",
+    ]
+
+    resources = ["${aws_s3_bucket.pipeline.arn}/*"]
+
+    condition {
+      test     = "StringNotEquals"
+      variable = "s3:x-amz-acl"
+      values   = ["bucket-owner-full-control"]
+    }
+  }
+
+  statement {
+    sid    = "DenyIncorrectEncryptionHeader"
+    effect = "Deny"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:PutObject",
+    ]
+
+    resources = ["${aws_s3_bucket.pipeline.arn}/*"]
+
+    condition {
+      test     = "StringNotEquals"
+      variable = "s3:x-amz-server-side-encryption"
+      values   = ["AES256"]
+    }
+  }
+
+  statement {
+    sid    = "DenyUnencryptedObjectUploads"
+    effect = "Deny"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:PutObject",
+    ]
+
+    resources = ["${aws_s3_bucket.pipeline.arn}/*"]
+
+    condition {
+      test     = "Null"
+      variable = "s3:x-amz-server-side-encryption"
+      values   = ["true"]
+    }
+  }
+
+  statement {
+    sid    = "DenyInsecureConnections"
+    effect = "Deny"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    actions = ["*"]
+
+    resources = [aws_s3_bucket.pipeline.arn]
+
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+
+      values = ["false"]
+    }
+  }
+}
