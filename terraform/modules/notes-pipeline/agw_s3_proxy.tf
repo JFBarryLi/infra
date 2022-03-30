fb0f48bc3208f87f6fd177fc45155d3a27f20c26
Barry <bli031@uottawa.ca>
2022-03-30 12:34:10 +0000

    Added agw method and integration response for s3 proxy.

--- terraform/modules/notes-pipeline/agw_s3_proxy.tf
+++ terraform/modules/notes-pipeline/agw_s3_proxy.tf
@@ -21,10 +21,10 @@ resource "aws_api_gateway_method" "s3_put" {
 }
 
 resource "aws_api_gateway_integration" "s3" {
-  http_method  = aws_api_gateway_method.s3_put.http_method
-  resource_id  = aws_api_gateway_resource.item.id
-  rest_api_id  = aws_api_gateway_rest_api.s3.id
-  type         = "AWS"
+  http_method = aws_api_gateway_method.s3_put.http_method
+  resource_id = aws_api_gateway_resource.item.id
+  rest_api_id = aws_api_gateway_rest_api.s3.id
+  type        = "AWS"
 
   integration_http_method = aws_api_gateway_method.s3_put.http_method
 
@@ -34,6 +34,78 @@ resource "aws_api_gateway_integration" "s3" {
   request_parameters = {
     "integration.request.path.key" = "method.request.path.item",
   }
+}
+
+resource "aws_api_gateway_method_response" "s3_200" {
+  rest_api_id = aws_api_gateway_rest_api.s3_.id
+  resource_id = aws_api_gateway_rest_api.s3_.root_resource_id
+  http_method = aws_api_gateway_method.s3_put.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Timestamp"      = true
+    "method.response.header.Content-Length" = true
+    "method.response.header.Content-Type"   = true
+  }
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+}
+
+resource "aws_api_gateway_method_response" "s3_400" {
+  depends_on = ["aws_api_gateway_integration.s3"]
+
+  rest_api_id = aws_api_gateway_rest_api.s3.id
+  resource_id = aws_api_gateway_rest_api.s3.root_resource_id
+  http_method = aws_api_gateway_method.s3_put.http_method
+  status_code = "400"
+}
+
+resource "aws_api_gateway_method_response" "s3_500" {
+  depends_on = ["aws_api_gateway_integration.s3"]
+
+  rest_api_id = aws_api_gateway_rest_api.s3.id
+  resource_id = aws_api_gateway_rest_api.s3.root_resource_id
+  http_method = aws_api_gateway_method.s3_put.http_method
+  status_code = "500"
+}
+
+resource "aws_api_gateway_integration_response" "s3_200" {
+  depends_on = ["aws_api_gateway_integration.s3"]
+
+  rest_api_id = aws_api_gateway_rest_api.s3.id
+  resource_id = aws_api_gateway_rest_api.s3.root_resource_id
+  http_method = aws_api_gateway_method.s3_put.http_method
+  status_code = aws_api_gateway_method_response.s3_200.status_code
+
+  response_parameters = {
+    "method.response.header.Timestamp"      = "integration.response.header.Date"
+    "method.response.header.Content-Length" = "integration.response.header.Content-Length"
+    "method.response.header.Content-Type"   = "integration.response.header.Content-Type"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "s3_400" {
+  depends_on = ["aws_api_gateway_integration.s3"]
+
+  rest_api_id = aws_api_gateway_rest_api.s3.id
+  resource_id = aws_api_gateway_rest_api.s3.root_resource_id
+  http_method = aws_api_gateway_method.s3_put.http_method
+  status_code = aws_api_gateway_method_response.s3_400.status_code
+
+  selection_pattern = "4\\d{2}"
+}
+
+resource "aws_api_gateway_integration_response" "s3_500" {
+  depends_on = ["aws_api_gateway_integration.s3"]
+
+  rest_api_id = aws_api_gateway_rest_api.s3.id
+  resource_id = aws_api_gateway_rest_api.s3.root_resource_id
+  http_method = aws_api_gateway_method.s3_put.http_method
+  status_code = aws_api_gateway_method_response.s3_500.status_code
+
+  selection_pattern = "5\\d{2}"
 }
 
 resource "aws_api_gateway_deployment" "s3" {
