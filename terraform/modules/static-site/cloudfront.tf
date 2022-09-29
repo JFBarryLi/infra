f7468235b71f745d7844aa721167c6cadc69911d
barry@barryli.ca <barry@barryli.ca>
2022-09-29 20:13:33 +0100

    Updated cf distribution to work with react router.

--- terraform/modules/static-site/cloudfront.tf
+++ terraform/modules/static-site/cloudfront.tf
@@ -66,6 +66,20 @@ resource "aws_cloudfront_distribution" "this" {
     Name      = var.domain_name
     ManagedBy = "terraform"
   }
+
+  custom_error_response {
+    error_caching_min_ttl = 3600
+    error_code = 404
+    response_code = 200
+    response_page_path = "/index.html"
+  }
+
+  custom_error_response {
+    error_caching_min_ttl = 3600
+    error_code = 403
+    response_code = 200
+    response_page_path = "/index.html"
+  }
 }
 
 data "aws_caller_identity" "current" {}
