64e348f7370ac34bea363d839322716833c521b2
barry <barry@barryli.ca>
2024-05-08 23:00:25 -0400

    New dynamodb table for adventure redesign.

--- terraform/modules/notes-pipeline/dynamo.tf
+++ terraform/modules/notes-pipeline/dynamo.tf
@@ -19,3 +19,25 @@ resource "aws_dynamodb_table" "travel_log" {
     enabled = true
   }
 }
+
+resource "aws_dynamodb_table" "adventure_log" {
+  name         = "AdventureLog"
+  billing_mode = "PAY_PER_REQUEST"
+
+  hash_key  = "PK"
+  range_key = "SK"
+
+  attribute {
+    name = "PK"
+    type = "S"
+  }
+
+  attribute {
+    name = "SK"
+    type = "S"
+  }
+
+  server_side_encryption {
+    enabled = true
+  }
+}
